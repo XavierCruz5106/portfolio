b2693763068ee04a8084bf5cc025367c93f49015
Xavier Cruz <68714224+XavierCruz5106@users.noreply.github.com>
2024-11-27 09:06:14 -0500

    feat: adds some boilerplate

---
+++ portfolio/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+export const Navbar = () => {
+  return (
+    <>
+      <div className="nav-container">
+        <ul className="nav-items">
+          <li className="nav-item">
+            <p>Home</p>
+          </li>
+          <li className="nav-item">
+            <p>About</p>
+          </li>
+          <li className="nav-item">
+            <p>Contact</p>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
